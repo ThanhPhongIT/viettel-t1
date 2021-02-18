63e756374c6ab23520cd85eb2f72c577689dff6d
Thanh Phong <phonga3bdq@gmail.com>
2021-02-18 19:06:11 +0700

    push

---
+++ version 3/assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.banner__carousel').owlCarousel({
+        loop: true,
+        items: 1,
+        dots: true,
+        mouseDrag: false,
+        touchDrag: false,
+    })
+    var selector = $('.banner__carousel');
+
+    $('.my-next-button').click(function() {
+        selector.trigger('next.owl.carousel');
+    });
+
+    $('.my-prev-button').click(function() {
+        selector.trigger('prev.owl.carousel');
+    });
+})
