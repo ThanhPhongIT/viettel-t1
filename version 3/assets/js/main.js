86615c165747379f2a4f3f9e2833916c2fc30c6c
Thanh Phong <phonga3bdq@gmail.com>
2021-02-20 21:57:16 +0700

    'fix'

--- version 3/assets/js/main.js
+++ version 3/assets/js/main.js
@@ -13,49 +13,59 @@ $(document).ready(function() {
         // Parameters has to be in square bracket '[]'
         owl.trigger('prev.owl.carousel', [300]);
     })
-});
-new WOW().init();
-var animateHTML = function() {
-    var elems, windowHeight;
-    var flag = false;
-    var init = function() {
-        elems = document.getElementsByClassName('total');
-        windowHeight = window.innerHeight;
-        _addEventHandlers();
-    };
-    var _addEventHandlers = function() {
 
-        window.addEventListener('scroll', _checkPosition);
+    function scrollShowImg(elmHover, elmShow) {
+        $(elmHover).hover(function() {
+            if (!$(elmShow).hasClass('animate')) {
+                $(elmShow).addClass('animate')
+            }
+        })
+    }
+    scrollShowImg('.service', '.img-grid1');
+
+    new WOW().init();
+    var animateHTML = function() {
+        var elems, windowHeight;
+        var flag = false;
+        var init = function() {
+            elems = document.getElementsByClassName('total');
+            windowHeight = window.innerHeight;
+            _addEventHandlers();
+        };
+        var _addEventHandlers = function() {
+
+            window.addEventListener('scroll', _checkPosition);
 
 
-    };
-    var _checkPosition = function() {
+        };
+        var _checkPosition = function() {
 
 
-        if (!flag) {
-            var posFromTop = elems[0].getBoundingClientRect().top;
-        } else {
-            posFromTop = 100000000000000;
-        }
-        if (posFromTop - windowHeight <= 0) {
-            $('.total').each(function() {
-                $(this).prop('Counter', 0).animate({
-                    Counter: $(this).text()
-                }, {
-                    duration: 2000,
-                    easing: 'swing',
-                    step: function(now) {
-                        $(this).text(Math.ceil(now));
-                    }
+            if (!flag) {
+                var posFromTop = elems[0].getBoundingClientRect().top;
+            } else {
+                posFromTop = 100000000000000;
+            }
+            if (posFromTop - windowHeight <= 0) {
+                $('.total').each(function() {
+                    $(this).prop('Counter', 0).animate({
+                        Counter: $(this).text()
+                    }, {
+                        duration: 2000,
+                        easing: 'swing',
+                        step: function(now) {
+                            $(this).text(Math.ceil(now));
+                        }
+                    });
                 });
-            });
-            flag = true;
-        }
+                flag = true;
+            }
 
+        };
+        return {
+            init: init,
+        };
     };
-    return {
-        init: init,
-    };
-};
 
-animateHTML().init();
+    animateHTML().init();
+});
