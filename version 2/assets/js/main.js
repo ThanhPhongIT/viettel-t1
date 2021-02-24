b30068b29b3cd14b2f882ee846d8d36d75a4f845
Thanh Phong <phonga3bdq@gmail.com>
2021-02-24 15:00:01 +0700

    'fix'

--- version 2/assets/js/main.js
+++ version 2/assets/js/main.js
@@ -1,7 +1,23 @@
 window.onload = function() {
     $('#overlay').addClass("hidden");
 }
-new WOW().init();
+WOW.prototype.addBox = function(element) {
+    this.boxes.push(element);
+};
+
+// Init WOW.js and get instance
+var wow = new WOW();
+wow.init();
+
+// Attach scrollSpy to .wow elements for detect view exit events,
+// then reset elements and add again for animation
+$('.wow').on('scrollSpy:exit', function() {
+    $(this).css({
+        'visibility': 'hidden',
+        'animation-name': 'none'
+    }).removeClass('animated');
+    wow.addBox(this);
+}).scrollSpy();
 
 var animateHTML = function() {
     var elems, windowHeight;
